999901e646816eae0f3d6909e5ef6ea9891733c5
almalrich <seb-bastos16@live.fr>
2018-11-20 15:52:39 +0100

    2eme commit

--- correction/chrono.js
+++ correction/chrono.js
@@ -1,27 +1,67 @@
-var heures=0;
-var minutes=1;
-var sec=5;
+var sec;
 
-document.getElementById("secon").innerHTML=sec;
-function ana (){
+var minutes = 0;
+
+var heures = 0;
+
+var x;
+
+
+function ana() {
+    if (heures == 0 && minutes == 0 && sec == 0) {
+        /*  clearTimeout(x);*/
+        alert('fini');
+
+
+    }
     sec--;
-    document.getElementById("secon").innerHTML=sec;
-        if(sec<1){
-            sec=60;
+
+    if (sec < 0) {
+        minutes = minutes - 1;
+        sec = 59;
+    }
+
+    if (minutes < 0) {
+        if (heures > 0) {
+            minutes = 59;
+            heures = heures - 1;
+        }
+        else {
+            minutes = 0;
         }
 
-
-document.getElementById("minute").innerText=minutes;
-        minutes--;
-        if(minutes<1){
-            minutes=60;
-        }
+    }
 
 
+    document.getElementById("secon").innerHTML = sec;
+    document.getElementById("minut").innerHTML = minutes;
+    document.getElementById("heure").innerHTML = heures;
 
 
-    setTimeout(ana,1000);
+    x = setTimeout(ana, 1000);
 }
-setTimeout(ana,1000);
+
+document.getElementById("tempfinal").addEventListener("click", function () {
+    heures = document.getElementById("heur").value;
+    minutes = document.getElementById("minu").value;
+    sec = document.getElementById("second").value;
+    ana();
+});
+
+document.getElementById("res").addEventListener("click", function () {
+    clearTimeout(x);
+    sec = 0;
+    minutes = 0;
+    heures = 0;
+});
+
+document.getElementById("rest").addEventListener("click", function () {
+    sec = 0;
+    document.getElementById("secon").innerHTML = sec;
+    minutes = 0;
+    document.getElementById("minut").innerHTML = minutes;
+    heures = 0;
+    document.getElementById("heur").innerHTML = heures;
 
 
+});
